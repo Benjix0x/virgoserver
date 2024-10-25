aefba782e08f916bb352a06db36e0fc2597d8a10
Benjix0x <110305962+Benjix0x@users.noreply.github.com>
2024-10-25 16:54:22 +0000

    Mundo guardado 2024-10-25

--- server.py
+++ server.py
@@ -23,14 +23,7 @@
 
 
 
-
-
-
-
-
-
-
-# No toques nada de aquí para abajo, puedes dañarlo
+# No tocar nada de aquí para abajo, pueden dañarlo
 import requests,os,base64,glob,time
 if os.path.exists("servidor.py"):
 	os.remove("servidor.py")
